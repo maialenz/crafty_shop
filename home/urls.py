5c66fd8019935db3ca657c4a387f075f40b15569
maialenz <maialenzumeta@gmail.com>
2021-12-01 15:19:41 +0000

    Add home app and templates

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+""" Home app URL Configuration"""
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+]
---
+++ home/views.py
@@ -0,0 +1,7 @@
+""" Views file to render data taken from models.py within the Home App"""
+from django.shortcuts import render
+
+
+def index(request):
+    """ A view to render the index page """
+    return render(request, 'home/index.html')
