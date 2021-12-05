659e5091e8bfb47d3e7130d08e97c3fb4e276428
maialenz <maialenzumeta@gmail.com>
2021-12-05 16:12:46 +0000

    Add product details functionality and updated nav links

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 """ Views file to render data taken from models.py within the Home App"""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -13,3 +13,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show all details belonging to a product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
