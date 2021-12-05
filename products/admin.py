bf08db32fe0f51b415729e7ffbab24179a3ab81e
maialenz <maialenzumeta@gmail.com>
2021-12-05 11:06:21 +0000

    Add products model for product and categories and models, loaded data

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
+''' Register Products and Category for admin '''
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
