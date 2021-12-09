89232b3593283e164ea6a4554fffa455db7d42de
maialenz <maialenzumeta@gmail.com>
2021-12-09 12:30:24 +0000

    Add a checkout view and updated project urls and checkout urls

---
+++ checkout/urls.py
@@ -0,0 +1,8 @@
+""" Home app URL Configuration"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.checkout, name='checkout'),
+]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+''' Checkout view '''
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .form import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('services'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
