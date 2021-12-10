ecf9882ee414800482b08f80b7e7c8ed4a3fd99f
maialenz <maialenzumeta@gmail.com>
2021-12-10 10:50:16 +0000

    add basic stripe checkout functionality and moved stripe elements js to checkout app

--- crafty_shop/settings.py
+++ crafty_shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
@@ -186,3 +187,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Variables to calculate Delivery cost
 FREE_DELIVERY_THRESHOLD = 25
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+# Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
