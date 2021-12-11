59bdaff787dca5ab2a06505b095f3a047c6a810f
maialenz <maialenzumeta@gmail.com>
2021-12-11 10:42:31 +0000

    Add order history logic to checkout and profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
+''' Profile App views '''
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from checkout.models import Order
+
 from .models import UserProfile
 from .forms import UserProfileForm
-
-from checkout.models import Order
 
 
 @login_required
@@ -19,8 +20,8 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(request, 'Update failed. \
-                           Please ensure the form is valid.')
+            messages.error(request,
+                           'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -34,3 +35,19 @@ def profile(request):
 
     return render(request, template, context)
 
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
