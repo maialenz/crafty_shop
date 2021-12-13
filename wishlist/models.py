cbc4f830edd5178f4f6fea179496274ea6605d6d
maialenz <maialenzumeta@gmail.com>
2021-12-13 20:43:16 +0000

    apply migrations to wishlist models

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,27 @@
+''' Wishlist app models '''
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+from profiles.models import UserProfile
+
+
+class Wishlist(models.Model):
+    products = models.ForeignKey(Product, null=False, blank=False,
+                                 on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(
+        Wishlist, null=False, blank=False, on_delete=models.CASCADE,
+        related_name='wishlist_items')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE,
+        related_name='wishlist_products')
+
+    def __str__(self):
+        return self.product.name
