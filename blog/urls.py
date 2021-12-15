7998933f5b493a40f8e2b187dc22408e232bed02
maialenz <maialenzumeta@gmail.com>
2021-12-15 19:46:55 +0000

    add urls to blog urls to connect delete, add comment. delete comment ...

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path('<int:post_id>/', views.post_detail, name='post_detail'),
     path('add/', views.add_post, name='add_post'),
     path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
     path('managecategories/', views.all_postcategories, name='postcategories'),
     path('addpostcategory/', views.add_postcategory, name='add_postcategory'),
+    path('editpostcategory/<int:postcategory_id>/', views.edit_postcategory,
+         name='edit_postcategory'),
+    path('deletepostcategory/<int:postcategory_id>/',
+         views.delete_postcategory, name='delete_postcategory'),
+    path('deletecomment/<int:comment_id>/', views.delete_comment,
+         name='delete_comment'),
 ]
