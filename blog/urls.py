e76adf805b5f0d6f11ecd0cf14b4363091d690d7
maialenz <maialenzumeta@gmail.com>
2021-12-15 14:31:19 +0000

    update all urls

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.all_posts, name='posts'),
     path('<int:post_id>/', views.post_detail, name='post_detail'),
     path('add/', views.add_post, name='add_post'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('managecategories/', views.all_postcategories, name='postcategories'),
+    path('addpostcategory/', views.add_postcategory, name='add_postcategory'),
 ]
