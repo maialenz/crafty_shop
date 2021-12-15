4883bf9814b17c4c61d4321d0d35e37ef97f79bf
maialenz <maialenzumeta@gmail.com>
2021-12-15 20:14:42 +0000

    add int to url product_id

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('review/<product_id>/', views.add_review,
-        name='add_review'),
+    path('review/<int:product_id>/', views.add_review,
+         name='add_review'),
 ]
