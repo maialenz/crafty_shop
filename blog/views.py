c964a477a9a644b17c066543c5324f12aa6ec025
maialenz <maialenzumeta@gmail.com>
2021-12-15 09:35:43 +0000

    add view all post  and post  details views

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,53 @@
-from django.shortcuts import render, reverse, get_object_or_404
+''' Views to render Blog models '''
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
-from .models import BlogPost
+from .models import Post, Postcategory, Comment
+from .forms import PostForm, PostcategoryForm, CommentForm
 
 
-def all_blog_posts(request):
-    """
-    View all blog posts on template 
-    """
-    blogs = BlogPost.objects.all()
+def all_posts(request):
+    """ A view to show all blog posts """
+
+    posts = Post.objects.all()
+    postcategories = Postcategory.objects.all()
+
+    if request.GET:
+        if 'postcategory' in request.GET:
+            postcategories = request.GET['postcategory'].split(',')
+            posts = posts.filter(postcategory__name__in=postcategories)
+            postcategories = Postcategory.\
+                objects.filter(name__in=postcategories)
 
     context = {
-        'blogs': blogs,
+        'posts': posts,
+        'postcategories': postcategories,
     }
 
-    return render(request, 'blog/blog.html', context)
+    return render(request, 'blog/posts.html', context)
+
+
+def post_detail(request, post_id):
+    template_name = 'posts/post_detail.html'
+    post = get_object_or_404(Post, pk=post_id)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
+
