59d880e5eea1fb0b151c30989edaeb778a6c7e4f
maialenz <maialenzumeta@gmail.com>
2021-12-16 01:23:23 +0000

    Refactor, styled code to fix mobile view including smallest

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -8,8 +8,10 @@
 
 let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 let clientSecret = $('#id_client_secret').text().slice(1, -1);
+
 let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
+
 let style = {
     base: {
         color: '#000',
@@ -25,6 +27,7 @@ let style = {
         iconColor: '#dc3545'
     }
 };
+
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
 
